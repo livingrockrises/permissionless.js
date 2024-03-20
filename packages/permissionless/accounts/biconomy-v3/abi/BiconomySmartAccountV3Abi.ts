c27170281f77b9f3987b2128320c2170a34214f6
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2024-03-20 04:48:49 +0400

    WIP

---
+++ packages/permissionless/accounts/biconomy-v3/abi/BiconomySmartAccountV3Abi.ts
@@ -0,0 +1,9 @@
+/**
+ * The exeuctor abi, used to execute transactions on the Biconomy ERC7579 Modular Smart Account
+ */
+export const BiconomyExecuteAbi = [] as const
+
+/**
+ * The init abi, used to initialise Biconomy Modular Smart Account / setup default ECDSA module
+ */
+export const BiconomyInitAbi = [] as const
